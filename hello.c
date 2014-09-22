0b54f03ef0b675eef6416164b9d806876605f9bc
Sahar Abbaspour <sabb@dtu.dk>
2014-09-22 12:35:13 +0200

    spill and fill work fine

--- hello.c
+++ hello.c
@@ -11,46 +11,62 @@ extern int SWSC_SPM_SIZE;
 
 #define MASK (SWSC_SPM_SIZE - 1)
 
+// define to poison SPM when spilling word
+#define POISON
+// define to disable filling (check if poisoned value comes back)
+// undefine to test whether spill/fill works
+#define NOENSURE
+
+
 #if 1
 void _sc_reserve() __attribute__((naked,used));
 void _sc_reserve()
 {
 
-  int sc_top_tmp, m_top, sc_top1;
+  unsigned sc_top_tmp, m_top, sc_top1;
   int   i, n, n_spill;
   int spilled_word;
   asm volatile("mov %0 = $r1;" // copy argument to n
       "mov %1 = $r27;" // copy st to sc_top
       "mov %2 = $r28;" // copy ss to m_top
-      : "=r" (n), "=r"(sc_top1), "=r"(m_top) /* output regs */
+      : "=r" (n), "=r"(sc_top1), "=r"(m_top) 
       ::
       );
 
-  _SPM int *spm = (_SPM int *) (sc_top1 & MASK);
-  _UNCACHED int *ext_mem = (_UNCACHED int *) m_top;
+  _SPM char *spm = (_SPM char *) ((m_top-1) & MASK);
+  _UNCACHED char *ext_mem = (_UNCACHED char *) (m_top-1);
 
-  n_spill = m_top - sc_top1 - _spm_ext_diff + n - SWSC_SPM_SIZE; 
+  n_spill = m_top - sc_top1 - _spm_ext_diff + n * 4 - SWSC_SPM_SIZE; 
   n_spill = n_spill / 4; // convert to words
   sc_top_tmp = sc_top1;
-  sc_top_tmp -= n ;
+  sc_top_tmp -= n * 4;
   sc_top1 = sc_top_tmp;
   
-  spilled_word = *ext_mem--; // m_top --
+ 
 
   for (i = 0; i < n_spill; i++){
-    m_top --;
-    spilled_word = *spm--;
-    *ext_mem-- = spilled_word;
+    m_top -= 4;
+#ifdef POISON
+ spilled_word = *spm;
+ *spm-- = -1;
+#else
+    spilled_word = *spm;
+#endif
+    *ext_mem = spilled_word;
+    ext_mem--; 
+    spm--;
   }
 
 
   asm volatile(
       "mov $r27 = %0;" // copy sc_top to st
       "mov $r28 = %1;" // copy m_top to ss
-      : /* no output regs */
-      : "r"(sc_top1), "r"(m_top) /* input regs */
-      : "$r27", "$r28" /* clobbered */
+      : 
+      : "r"(sc_top1), "r"(m_top) 
+      : "$r27", "$r28" 
       );
+
+
 }
 #endif
 
@@ -59,7 +75,7 @@ void _sc_ensure() __attribute__((naked,used));
 void _sc_ensure()
 {
 
-  int m_top, sc_top1;
+  unsigned m_top, sc_top1;
   int n, i, n_fill, filled_word;;
   int r1, r2;
   asm volatile(
@@ -68,20 +84,24 @@ void _sc_ensure()
       "mov %2 = $r8;" // copy argument to n
       "mov %3 = $r27;" // copy st to sc_top
       "mov %4 = $r28;" // copy ss to m_top
-      : "=r" (r1), "=r" (r2), "=r" (n), "=r"(sc_top1), "=r"(m_top) /* output regs */
+      : "=r" (r1), "=r" (r2), "=r" (n), "=r"(sc_top1), "=r"(m_top) 
       ::
       );
 
-  _SPM int *spm = (_SPM int *) (sc_top1 & MASK);
-  _UNCACHED int *ext_mem = (_UNCACHED int *) m_top;
+  _SPM char *spm = (_SPM char *) ((m_top - 1) & MASK);
+  _UNCACHED char *ext_mem = (_UNCACHED char *) (m_top -1);
 
-  n_fill = n - (m_top - sc_top1 - _spm_ext_diff); 
+  n_fill = n * 4 - (m_top - sc_top1 - _spm_ext_diff); 
   n_fill = n_fill / 4; // convert to words
 
   for (i = 0; i < n_fill; i++){
-    m_top++;
-    filled_word = *ext_mem++;
-    *spm++ = filled_word;
+    m_top += 4;
+    filled_word = *ext_mem;
+#ifndef NOENSURE
+    *spm = filled_word;
+#endif
+    ext_mem++;
+    spm++;
   }
 
 
@@ -90,10 +110,12 @@ void _sc_ensure()
       "mov $r28 = %1;" // m_top
       "mov $r1 = %2;" // restore r1
       "mov $r2 = %3;" // restore r2
-      : /* no output regs */
-      : "r"(sc_top1), "r"(m_top), "r"(r1), "r"(r2) /* input regs */
-      : "$r1", "$r2", "$r27", "$r28" /* clobbered */
+      : 
+      : "r"(sc_top1), "r"(m_top), "r"(r1), "r"(r2) 
+      : "$r1", "$r2", "$r27", "$r28"
       );
+
+
 }
 #endif
 
@@ -103,8 +125,8 @@ void _sc_free() __attribute__((naked,used));
 void _sc_free()
 {
 
-  int sc_top, r1, r2, m_top, n;
-
+  int r1, r2, n;
+  unsigned sc_top, m_top;
 
   asm volatile(
       "mov %0 = $r1;" // save r1
@@ -116,7 +138,7 @@ void _sc_free()
       ::
       );
 
-  sc_top += n;
+  sc_top += n * 4;
    
   if ((sc_top + _spm_ext_diff) > m_top) {
 	m_top = sc_top + _spm_ext_diff;
@@ -156,34 +178,59 @@ void _sc_store()
 #endif
 
 
-int main(int argc, char **argv) {
+void call1() {
   
- puts("Hello world.\n"); 
- int st_val = 10;
-  int sc_top1;
-
-  asm volatile(
-      "mov %0 = $r27;" // copy st to sc_top
-      : "=r"(sc_top1) 
-      ::
-      );
-
+  int sc_top1 = 12;
+  int st_val1 = 10;
+  int st_val;
   _SPM int *spm = (_SPM int *) sc_top1;
-
-  *spm = st_val;
-  puts("Hello world.\n");
-
-
-  asm volatile(
-      "mov %0 = $r27;" // copy st to sc_top
-      : "=r"(sc_top1)
-      ::
-      );
-
-  st_val =  *spm;
-
+  *spm = st_val1;
+  st_val = *spm;
   printf("0x%x\n", st_val);
 
+
+}
+
+void call2() {
+  
+  int sc_top1 = 12;
+  int st_val1 = 10;
+  int st_val;
+  _SPM int *spm = (_SPM int *) sc_top1;
+  *spm = st_val1;
+  st_val = *spm;
+  printf("0x%x\n", st_val);
+
+
+}
+
+
+void recursion(int i) {
+ if (i > 0)
+ recursion(--i);
+ puts("rec");
+}
+
+int main(int argc, char **argv) {
+  
+ 
+  int sc_top1;
+  int st_val1 = 10;
+//  int st_val;
+
+ // puts("Hello world.\n"); 
+
+//  volatile _SPM unsigned *spm = (_SPM unsigned *) sc_top1;
+//  *spm = st_val1;
+
+ // printf("0x%x\n", *spm);
+ // call1();
+ // call2();
+ // st_val = *spm;
+ // printf("0x%x\n", *spm);
+
+// call recursive function to deplete stack cache and cause spilling
+ recursion(10);
 
   return 0;
 }
